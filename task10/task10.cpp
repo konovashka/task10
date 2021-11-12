e2f74c1714603dfd1c67763444918ed347387654
konovashka <anton_konovalov_1@mail.ru>
2021-11-12 19:28:49 +0300

    Добавьте файлы проекта.

---
+++ task10/task10.cpp
@@ -0,0 +1,39 @@
+﻿// Удаление из последовательности 0 и проверка на чередование знаков
+
+#include <iostream>
+
+int main()
+{
+    int n,x,prev,fl;
+	fl = 1;
+	scanf_s("%d", &n);
+	scanf_s("%d", &prev);
+	for (int i = 0; ((i < n-1)&&(fl==1)); i++)
+	{
+		scanf_s("%d", &x);
+		if (x!=0)
+		{
+			if (((x<0)&&(prev>0))||((x>0)&&(prev<0)))
+			{
+				fl = 1;
+				prev = x;
+			}
+			else
+			{
+				fl = 0;
+			}
+		}
+		else
+		{
+			i -= 1;
+		}
+	}
+	if (fl==1)
+	{
+		printf("yavlyaetsya");
+	}
+	else
+	{
+		printf("ne yavlyaetsya");
+	}
+}
